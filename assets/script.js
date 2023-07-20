a4eb8d6b90c37abc68ea878dd29874aecdc805a3
vicdan7 <ibarravictor334@gmail.com>
2023-07-20 15:30:36 -0500

    responsive

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,38 @@
-function selection(link){
-    const options = document.querySelectorAll('#links a');
-    options[0].className = "";
-    options[1].className = "";
-    options[2].className = "";
-    options[3].className = "";
-    options[4].className = "";
-    link.className = "selector";
+function selection(link) {
+  const options = document.querySelectorAll("#links a");
+  options[0].className = "";
+  options[1].className = "";
+  options[2].className = "";
+  options[3].className = "";
+  options[4].className = "";
+  link.className = "selector";
 
-    let x = document.getElementById("nav");
+  let x = document.getElementById("nav");
+  x.className = "";
+}
+
+function responsiveMenu() {
+  let x = document.getElementById("nav");
+  if (x.className === "") {
+    x.className = "responsive";
+  } else {
     x.className = "";
-
+  }
 }
 
-function responsiveMenu(){
-    let x = document.getElementById("nav");
-    if(x.className === ""){
-        x.className = "responsive";
-    }else{
-        x.className = "";
-    }
-}
-
-window.onscroll = function(){
-    efectSkills()
+window.onscroll = function () {
+  efectSkills();
 };
 
-function efectSkills(){
-    const skills = document.getElementById("skills");
-    const distance_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distance_skills >= 300){
-        document.getElementById("html&css").classList.add("set-progress1");
-        document.getElementById("js").classList.add("set-progress2");
-        document.getElementById("react").classList.add("set-progress3");
-        document.getElementById("git&github").classList.add("set-progress4");
-        document.getElementById("node").classList.add("set-progress5");
-    }
-
+function efectSkills() {
+  const skills = document.getElementById("skills");
+  const distance_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distance_skills >= 300) {
+    document.getElementById("html").classList.add("set-progress1");
+    document.getElementById("js").classList.add("set-progress2");
+    document.getElementById("react").classList.add("set-progress3");
+    document.getElementById("git").classList.add("set-progress4");
+    document.getElementById("node").classList.add("set-progress5");
+  }
 }
